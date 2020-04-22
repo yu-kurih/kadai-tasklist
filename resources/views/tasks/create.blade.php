df5d812e7cd01f5daa35193f11dbce05a45d445e
yu-kurih <yu.k.development@gmail.com>
2020-04-22 16:44:18 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -10,6 +10,12 @@
             {!! Form::model($task, ['route' => 'tasks.store']) !!}
         
                 <div class="form-group">
+                    {!! Form::label('status', 'status:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
+        
+                <div class="form-group">
                     {!! Form::label('content', 'タスク名:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
