adffa0a5554a60cce05ca5469c247ce18898e14b
yu-kurih <yu.k.development@gmail.com>
2020-04-22 11:05:48 +0000

    kadai-tasklist

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+
+    <h1>タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>タスク一覧</th>
+                </tr>
+            </thead>
+            <tbody>
+             @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+    {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-primary']) !!}
+
+
+    @endif
+
+
+@endsection
